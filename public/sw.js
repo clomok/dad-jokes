65070d82c73d1d4d916c91c102ad7dd4f1614dc8
clomok <96883102+clomok@users.noreply.github.com>
2026-04-24 08:25:36 -0700

    Replace service worker with self-cleaning version to fix blank page

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,9 @@
-const CACHE = 'dad-jokes-v2';
-const ASSETS = ['.', 'index.html', 'manifest.json', 'icon.svg'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
 });
